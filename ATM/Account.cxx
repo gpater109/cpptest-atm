3382ce2cebdbec33d5ac22500fb24362dbb717e6
gpater109 <gpater@parasoft.com>
2025-04-11 09:38:36 +0200

    1

--- ATM/Account.cxx
+++ ATM/Account.cxx
@@ -6,8 +6,6 @@ double Account::deposit(double amount)
     return (getBalance());
 }
 
-void foo1( int a, int b );           // Violation
-void foo1( int a, int b ) {}
 
 double Account::debit(double amount)
 {
